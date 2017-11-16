b856a2805a5dc1b9201d75abf22107654a84834c
ernovnik <ernovnik@gmail.com>
2017-11-16 09:21:17 +0300

    Update SchedulerA.scala

--- src/main/scala/ru/ernovnik/SchedulerA.scala
+++ src/main/scala/ru/ernovnik/SchedulerA.scala
@@ -1,4 +1,4 @@
-package ru.ernovnik
+package com.scalafirst.akka.sheduling
 
 import java.time.{LocalDateTime, ZoneOffset}
 import java.util.concurrent.Callable
@@ -7,59 +7,51 @@ import akka.actor.{Actor, ActorLogging, ActorRef, Cancellable, Props}
 
 import scala.collection.mutable.ListBuffer
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration.{FiniteDuration, _}
+import scala.concurrent.duration._
 
-case class Task(time: Long, callable: Callable[String]) {
-  def delay: FiniteDuration = {
-    val d = math.max(time - LocalDateTime.now.toInstant(ZoneOffset.UTC).toEpochMilli, 0)
-    d millis
-  }
+case class Task(localTime: LocalDateTime, callable: Callable[String]) {
+  val time: Long = localTime.toInstant(ZoneOffset.UTC).toEpochMilli
 
-//  def ready: Boolean = time < LocalDateTime.now.toInstant(ZoneOffset.UTC).toEpochMilli
+  def delay: FiniteDuration = math.max(time - LocalDateTime.now.toInstant(ZoneOffset.UTC).toEpochMilli, 0) millis
 }
 
 case object Execute
 
-
-object SchedulerA {
-  def props: Props = Props(new SchedulerA())
-}
-
 class SchedulerA extends Actor with ActorLogging {
 
-  val executor: ActorRef = context.system.actorOf(ExecutorA.props)
+  val executor: ActorRef = context.system.actorOf(Props[ExecutorA])
 
-  val q: ListBuffer[Task] = ListBuffer.empty
+  val queue: ListBuffer[Task] = ListBuffer.empty
 
   var scheduled: Option[Cancellable] = None
 
   def addToQueue(task: Task) {
-    val afterIndex = q.indexWhere(_.time > task.time)
-    if (afterIndex >= 0) {
-      // Добавляется перед другими задачами
-      q.insert(afterIndex, task)
-    } else {
-      // Добавляется в конец
-      q.append(task)
-    }
-    reschedule()
+    val biggerIndex = queue.indexWhere(_.time > task.time)
+    val insertIndex = if (biggerIndex == -1) queue.length // В конец очереди, если не найдено бОльшее значение
+    else biggerIndex // Добавляется перед другими задачами
+
+    queue insert(insertIndex, task)
+    if (insertIndex == 0) reschedule() // Если добавлено в голову - переопределяем следующий запуск
   }
 
   def reschedule() {
     scheduled.foreach(_.cancel())
-    scheduled = for (task <- q.headOption) yield
+    scheduled = for (task <- queue.headOption) yield
       context.system.scheduler.scheduleOnce(task.delay, self, Execute)
   }
 
-  override def receive: Receive = {
-    case (time: LocalDateTime, callable: Callable[String]) =>
-      val task = Task(time.toInstant(ZoneOffset.UTC).toEpochMilli, callable)
-      addToQueue(task)
-    case Execute =>
-      for (head <- q.headOption) {
-        q.trimStart(1)
-        reschedule()
-        executor ! head.callable
-      }
+  def execute() {
+    for (head <- queue.headOption) {
+      queue trimStart 1
+      reschedule()
+
+      executor ! head.callable
+    }
   }
+
+  override def receive: Receive = {
+    case task: Task => addToQueue(task)
+    case Execute => execute()
+  }
+
 }
